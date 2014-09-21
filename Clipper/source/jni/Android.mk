c6508b18350fb01f1cfc00eca7ad407edd34e3cd
Arturs Sosins <ar2rsawseen@yahoo.com>
2014-09-21 15:38:32 +0300

    Added Clipper plugin

---
+++ Clipper/source/jni/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+#
+# Gideros Shared Library
+# 
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := gideros
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libgideros.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+#
+# Plugin
+#
+include $(CLEAR_VARS)
+
+LOCAL_MODULE           := clipper
+LOCAL_ARM_MODE         := arm
+LOCAL_CFLAGS           := -O2
+LOCAL_SRC_FILES        := clipper.cpp clipperbinder.cpp
+LOCAL_LDLIBS           := -ldl -llog
+LOCAL_SHARED_LIBRARIES := gideros
+
+include $(BUILD_SHARED_LIBRARY)
