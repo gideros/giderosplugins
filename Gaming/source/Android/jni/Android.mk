4677107927179fb33f69681321013e02793cfb2d
Arturs Sosins <ar2rsawseen@yahoo.com>
2014-09-21 15:10:07 +0300

    Added Gaming plugin

---
+++ Gaming/source/Android/jni/Android.mk
@@ -0,0 +1,59 @@
+LOCAL_PATH := $(call my-dir)
+
+#
+# Gideros Shared Library
+# 
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := lua
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/liblua.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := gideros
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libgideros.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := gvfs
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libgvfs.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := zlib
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libzlib.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+#
+# Plugins
+#
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE           := gaming
+LOCAL_ARM_MODE         := arm
+LOCAL_CFLAGS           := -O2
+LOCAL_C_INCLUDES       += $(LOCAL_PATH)/../../libs/include
+LOCAL_SRC_FILES        := gamification.cpp gamificationbinder.cpp
+LOCAL_LDLIBS           := -ldl -llog
+LOCAL_SHARED_LIBRARIES := gideros lua
+
+include $(BUILD_SHARED_LIBRARY)
