0a15a8502649c44b0c208d9afd7e132a8f4d5255
Arturs Sosins <ar2rsawseen@yahoo.com>
2014-09-27 14:42:25 +0300

    Added Lua Struct plugin

---
+++ Struct/source/jni/Android.mk
@@ -0,0 +1,86 @@
+LOCAL_PATH := $(call my-dir)
+
+#
+# Gideros Shared Library
+# 
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := lua
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/liblua.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := gideros
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libgideros.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := gvfs
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libgvfs.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := zlib
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libzlib.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := json
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libjson.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := bitop
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libbitop.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE            := luasocket
+LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libluasocket.so
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+#
+# Plugins
+#
+
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE           := struct
+LOCAL_ARM_MODE         := arm
+LOCAL_CFLAGS           := -O2
+LOCAL_C_INCLUDES       += $(LOCAL_PATH)/../../libs/include
+LOCAL_SRC_FILES        := struct.c struct_stub.cpp
+LOCAL_LDLIBS           := -ldl -llog
+LOCAL_SHARED_LIBRARIES := lua gideros
+
+include $(BUILD_SHARED_LIBRARY)
