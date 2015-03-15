719eae92882f20ffe7362403eb9c2665ccf88ef9
A1NE <nzrkzk@gmail.com>
2015-03-15 20:20:34 +0200

    Adding Appodeal plugin

---
+++ Ads/example/GiderosProject/appodeal.lua
@@ -0,0 +1,28 @@
+ads = Ads.new("appodeal")
+--note:Bundle must match bundle_id in your app on Appodeal.com
+ads:setKey("fee50c333ff3825fd6ad6d38cff78154de3025546d47a84f")
+ads:showAd("auto")
+
+ads:addEventListener(Event.AD_RECEIVED, function()
+	print("ads AD_RECEIVED")
+end)
+
+ads:addEventListener(Event.AD_FAILED, function(e)
+	print("ads AD_FAILED", e.error)
+end)
+
+ads:addEventListener(Event.AD_ACTION_BEGIN, function()
+	print("ads AD_ACTION_BEGIN")
+end)
+
+ads:addEventListener(Event.AD_ACTION_END, function()
+	print("ads AD_ACTION_END")
+end)
+
+ads:addEventListener(Event.AD_DISMISSED, function()
+	print("ads AD_DISMISSED")
+end)
+
+ads:addEventListener(Event.AD_ERROR, function(e)
+	print("ads AD_ERROR", e.error)
+end)
